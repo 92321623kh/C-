217b67d0bce62529ba06cfa9cb2acab3f6a99081
92321623kh <huisrt3813@pasco.o.jp>
2020-08-19 15:57:35 +0900

    文字入力

--- Program.cs
+++ Program.cs
@@ -10,17 +10,12 @@ namespace Sample102
     {
         static void Main(string[] args)
         {
-            int a;
-            int b = 3;
-            int add, sub;
-            double avg;
-            a = 6;
-            add = a + b;
-            sub = a - b;
-            avg = (a + b) / 2.0;
-            Console.WriteLine("{0} + {1} = {2}", a, b, add);
-            Console.WriteLine("{0} - {1} = {2}", a, b, sub);
-            Console.WriteLine("{0}と{1}の平均値{2}", a, b, avg);
+            string str1, str2;
+            Console.Write("str1=");
+            str1 =Console.ReadLine();
+            Console.Write("str2=");
+            str2 = Console.ReadLine();
+            Console.WriteLine("str1 + str2 = {0}",str1 + str2);
         }
     }
-}
+} 
