952235670d5c3acb0a943007b335525731fd14f1
92321623kh <huisrt3813@pasco.o.jp>
2020-08-20 14:28:18 +0900

    ジャグ配列

--- Program.cs
+++ Program.cs
@@ -10,23 +10,20 @@ namespace Sample102
     {
         static void Main(string[] args)
         {
-            int[,] a = new int[3,4];
-            int m, n;
-            for (m = 0; m < 3; m++)
+            int[][] a = new int[][] {
+                        new int[] { 0, 1 },
+                        new int[] { 2 },
+                        new int[] { 3, 4, 5, 6}
+                        };
+            
+            for (int m = 0; m < a.Length; m++)
             {
-                for (n = 0; n < 4; n++)
+                for (int n = 0; n < a[m].Length; n++)
                 {
-                    a[m, n] = m + n;
+                    Console.Write(a[m][n] + "");
                 }
+                Console.WriteLine();
             }
-            for (m = 0; m < 3; m++)
-            {
-                for (n = 0; n < 4; n++)
-                {
-                    Console.Write("a[{0},{1}]={2}",m, n, a[m,n]);
-                }
-            }
-            Console.WriteLine();
         }
     }
 } 
