4a4b94ee814a9b54771c5456f1834ec0acb21387
92321623kh <huisrt3813@pasco.o.jp>
2020-08-22 10:45:23 +0900

    a

---
+++ keishou/ExCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keishou1
+{
+    class ExCalculator : keishou1_1
+    {
+        public void mul()
+        {
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+        }
+        public void div()
+        {
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+        }
+    }
+}
---
+++ keishou/keishou_program1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keishou1
+{
+    class keishou_program1
+    {
+        static void Main(string[] args)
+        {
+            ExCalculator c1 = new ExCalculator();
+            c1.Num1 = 10;
+            c1.Num2 = 3;
+
+            c1.add();
+            c1.sub();
+            ExCalculator c2 = new ExCalculator();
+            c2.Num1 = 10;
+            c2.Num2 = 3;
+            c2.add();
+            c2.sub();
+
+            c2.mul();
+            c2.div();
+        }
+    }
+}
