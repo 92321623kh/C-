21d52e77a763b8b80145e7d36a6b87818c981bea
92321623kh <huisrt3813@pasco.o.jp>
2020-08-22 11:46:55 +0900

    継承３

---
+++ sample502/sample502/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample502
+{
+    class Program
+    {
+       public virtual void Foo()
+       {
+            Console.WriteLine("親クラスのFoo()メソッド");
+       }
+    }
+}
