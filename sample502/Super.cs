fedc491caf686ffe61884d1d833dc62812f63842
92321623kh <huisrt3813@pasco.o.jp>
2020-08-22 11:12:31 +0900

    継承２

---
+++ sample502/Super.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample502
+{
+    class Super
+    {
+        private int param = 0;
+
+        public Super()
+        {
+            Console.WriteLine("Superクラスのコンストラクタ(引数なし)");
+        }
+        public Super(int param)
+        {
+            Console.WriteLine("Superクラスのコンストラクタ(引数:param={0})", param);
+            this.param = param;
+        }
+        ~Super()
+        {
+            Console.WriteLine("Superクラスのデスストラクタ");
+        }
+        public void showParam()
+        {
+            Console.WriteLine("param = {0}", param);
+        }
+    }
+}
